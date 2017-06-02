1308d89075c797d3b796d9b081d7e5401a981a7f
IceDcap <dousq0409@126.com>
2017-06-02 10:08:49 +0800

    Add and Search Word - Data structure design

---
+++ src/com/icedcap/leetcode/design/S211.java
@@ -0,0 +1,67 @@
+package com.icedcap.leetcode.design;
+
+/**
+ * Add and Search Word - Data structure design(M)
+ * <p>
+ * Design a data structure that supports the following two operations:
+ * <p>
+ * void addWord(word)
+ * bool search(word)
+ * search(word) can search a literal word or a regular expression string containing only letters a-z or .. A . means it can represent any one letter.
+ * <p>
+ * For example:
+ * <p>
+ * addWord("bad")
+ * addWord("dad")
+ * addWord("mad")
+ * search("pad") -> false
+ * search("bad") -> true
+ * search(".ad") -> true
+ * search("b..") -> true
+ */
+public class S211 {
+    private TrieNode root = new TrieNode();
+
+    /** Initialize your data structure here. */
+    public S211() {
+
+    }
+
+    /** Adds a word into the data structure. */
+    public void addWord(String word) {
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            if (node.children[c - 'a'] == null) {
+                node.children[c - 'a'] = new TrieNode();
+            }
+            node = node.children[c - 'a'];
+        }
+        node.item = word;
+    }
+
+    /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
+    public boolean search(String word) {
+        return match(word.toCharArray(), 0, root);
+    }
+
+    private boolean match(char[] chs, int k, TrieNode node) {
+        if (k == chs.length) return !node.item.equals("");
+        if (chs[k] != '.') {
+            return node.children[chs[k] - 'a'] != null && match(chs, k + 1, node.children[chs[k] - 'a']);
+        } else {
+            for (int i = 0; i < node.children.length; i++) {
+                if (node.children[i] != null) {
+                    if (match(chs, k + 1, node.children[i])) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    class TrieNode {
+        TrieNode[] children = new TrieNode[26];
+        String item = "";
+    }
+}
